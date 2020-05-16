2829cc464cc4ef3f560726171ebd1305b2086a12
pradlerfilip <filip.pradler@tul.cz>
2020-05-16 03:12:32 +0200

    Calendar finished

--- 07_Calendar/src/pkg07_calendar/Calendar.java
+++ 07_Calendar/src/pkg07_calendar/Calendar.java
@@ -14,14 +14,14 @@ package pkg07_calendar;
 public class Calendar {
     
      public static final int[] daysInMonth={31,28,31,30,31,30,31,31,30,31,30,31};
-     public static final String[] monthsInYear={"Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"};
+     public static final String[] monthsInYear={"January","February","March","April","May","June","July","August","September","October","November","December"};
      public static final String[] typeDaysInMonth={"Mon","Tue","Wed","Thu","Fri","Sat","Sun"};
 
     private int day;
     private int month;
     private int year;
 
-    public Calendar(int day, int month, int year) {
+    public Calendar(int month, int year) {
         this.day = day;
         this.month = month;
         this.year=year;
@@ -49,55 +49,75 @@ public class Calendar {
      
     private static int nOfDays(int month, int year)
     {
-        if(month == 1 && year % 4 == 0 )
+        if( (monthsInYear[month] == "Feb") && year % 4 == 0 && year % 100 != 0 )
         {
           return 29;
         }
         else
         {
-        return daysInMonth[month-1];
+        return daysInMonth[month];
         }
     }
     
     
 
-    private static int WhatDayIsIt(int day,int month, int year)
+    public static int WhatDayIsIt(int month, int year)
     {
-        int k=year % 100;
-        int j=year/100;
-        return (day + (13*(month+1)/5) + k + k/4 + j/4 + 5*j  ) % 7;
+        if (month < 3) {
+            month += 12;
+            year -= 1;
+        }
+        
+        int k = year % 100;
+        int j = year / 100;
+        
+        return (((26*(month+1)/10)+k+(k/4)+(j/4)+(5*j)) % 7)-1;
+        
+        
+        
     }
     
     
-   public void printCalendar()
+   public String getCalendar()
    {
-       System.out.println(getYear());
-       System.out.println(monthsInYear[getMonth()]);
+       StringBuilder sb = new StringBuilder();
+       //System.out.println("Year = " + getYear());
+       sb.append("Year = ").append(getYear());
+       //System.out.println("Month = " + monthsInYear[getMonth()-1]);
+       sb.append("\n");
+       sb.append("Month = ").append(monthsInYear[getMonth()-1]);
+       sb.append("\n");
        for(int i=0;i<7;i++)
        {
-           System.out.print("\t" + typeDaysInMonth[i]);
+           //System.out.print("\t" + typeDaysInMonth[i]);
+           sb.append(typeDaysInMonth[i]).append("\t");
        }
-       int firstDay=WhatDayIsIt(getDay(),getMonth(),getYear());
-       int nOfDaysInMonth=nOfDays(getMonth(),getYear());
-       System.out.println();
+       int firstDay=WhatDayIsIt(getMonth(),getYear());
+       int nOfDaysInMonth=nOfDays(getMonth()-1,getYear());
+       //System.out.println();
+        sb.append("\n");
        int written=0;
        while(written < firstDay) {
-       System.out.print("\t"); ++written;
+      // System.out.print("\t"); 
+       sb.append("\t");
+        ++written;
             }
-       while(written - firstDay <= nOfDays(getMonth(), getYear())) {
-            if(written % 7 == 0) System.out.print("\n");
-            System.out.print("\t" + (written - firstDay + 1));
+       while(written - firstDay < nOfDaysInMonth) {
+            if(written % 7 == 0)
+            {
+                // System.out.print("\n");
+                sb.append("\n");
+            }
+            //System.out.print("\t" + (written - firstDay + 1));
+            sb.append((written - firstDay + 1) + "\t");
             ++written;
        
 }
-       System.out.println();
+       // System.out.println();
+       sb.append("\n");
+       return sb.toString();
    }
-    
-
-    
-    
-    
-    
+ 
     
     
      
--- 07_Calendar/src/pkg07_calendar/main.java
+++ 07_Calendar/src/pkg07_calendar/main.java
@@ -5,6 +5,7 @@
  */
 package pkg07_calendar;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 /**
@@ -17,23 +18,150 @@ public class main {
     
      public static void main(String[] args)
     {
-        int day,month,year;
+        int month,year;
+        int m,n;
+        boolean end=false,end2=false;
+        while(!end)
+        {
+            System.out.println(getMenu());
+            n=sc.nextInt();
+           switch(n)
+           {
+               case 0:
+               end=true;
+               break;
+               case 1:
+                   System.out.print("What month do you want: ");
+                   month=sc.nextInt();
+                   System.out.print("What year do you want: ");
+                   year=sc.nextInt();
         
-        System.out.print("Zadej den: ");
-        day=sc.nextInt();
-        System.out.print("Zadej mesic: ");
+                   Calendar calendar = new Calendar(month,year);
+                   System.out.println(calendar.getCalendar());
+                   
+                   System.out.println(getActionMenu());
+                   m=sc.nextInt();
+                    while(!end2)
+                    {
+                   switch(m)
+                   {
+                       case 0:
+                           end=true;
+                           break;
+                       case 1:
+                           if(month != 12)
+                           {
+                           calendar = new Calendar(month+1,year);
+                           month++;
+                           }
+                           else
+                           {
+                               calendar = new Calendar(1,year+1);
+                               month=1;
+                               year++;
+                           }
+                           System.out.println(calendar.getCalendar());
+                           System.out.println(getActionMenu());
+                           m=sc.nextInt();
+                           break;
+                       case 2:
+                           if(month != 1)
+                           {
+                           calendar = new Calendar(month-1,year);
+                           month--;
+                           }
+                           else
+                           {
+                               calendar = new Calendar(12,year-1);
+                               month=12;
+                               year--;
+                           }
+                           System.out.println(calendar.getCalendar());
+                           System.out.println(getActionMenu());
+                            m=sc.nextInt();
+                           break;
+                       case 3:
+                           calendar = new Calendar(month,year+1);
+                           year++;
+                           System.out.println(calendar.getCalendar());
+                           System.out.println(getActionMenu());
+                           m=sc.nextInt();
+                           
+                           break;
+                       case 4:
+                           calendar = new Calendar(month,year-1);
+                           year--;
+                           System.out.println(calendar.getCalendar());
+                           System.out.println(getActionMenu());
+                           m=sc.nextInt();
+                           break;
+                       default:
+                             System.out.println("Wrong input");
+                             break;
+                   }
+                   
+                    }
+                   
+                   
+                   
+               case 2:
+                   System.out.print("What year do you want: ");
+                   year=sc.nextInt();
+                   ArrayList<Calendar> calendarYear = new ArrayList();
+                   for(int i=1; i<=12;i++)
+                   {
+                      calendarYear.add(new Calendar(i,year));
+                   }
+                   for(Calendar cal:calendarYear)
+                   {
+                       System.out.println(cal.getCalendar());
+                   }
+                   break;
+               default:
+                   System.out.println("Wrong input");
+                   
+                       
+           }
+            
+            
+         
+            
+      
+       /* System.out.print("Zadej mesic: ");
         month=sc.nextInt();
         System.out.print("Zadej rok: ");
         year=sc.nextInt();
         
-        Calendar calendar = new Calendar(day,month,year);
+        Calendar calendar = new Calendar(month,year);
         
-       
-        
-       
-            calendar.printCalendar();
-            calendar = new Calendar(day,month+1,year);
-            calendar.printCalendar();
-       
+      
+            //calendar.printCalendar();
+            System.out.println(calendar.getCalendar());
+            System.out.println("------------------------------------------------------");
+       */
+          
+        }
     }
+     
+     
+     public static String getMenu()
+     {
+         StringBuilder sb = new StringBuilder();
+         sb.append("0 for exit \n");
+         sb.append("1 for calendar with specific month and year \n");
+         sb.append("2 for whole calendar \n");
+         sb.append("What you want to do : ");
+         return sb.toString();
+     }
+     
+     public static String getActionMenu()
+     {
+       StringBuilder sb = new StringBuilder();
+         sb.append("0 for exit \n");
+         sb.append("1 for next month \n");
+         sb.append("2 for previous month \n");
+         sb.append("3 for next year \n");
+         sb.append("4 for next year");
+         return sb.toString();
+     }
 }
